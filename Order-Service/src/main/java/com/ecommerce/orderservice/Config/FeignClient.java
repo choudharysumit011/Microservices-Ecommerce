af31aa6618339e59a0e3cb5112168291a5868ba5
choudharysumit011 <csumit408@gmail.com>
2024-07-14 20:28:24 +0530

    Api Discovery Added

--- Order-Service/src/main/java/com/ecommerce/orderservice/Config/FeignClient.java
+++ Order-Service/src/main/java/com/ecommerce/orderservice/Config/FeignClient.java
@@ -2,16 +2,18 @@ package com.ecommerce.orderservice.Config;
 
 
 import com.ecommerce.orderservice.DTO.InventoryResponse;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@org.springframework.cloud.openfeign.FeignClient(name = "feign",url = "http://localhost:8082/api/inventory")
-public interface FeignClient {
+@org.springframework.cloud.openfeign.FeignClient(name = "inventory-service")
 
-    @GetMapping("/exists")
+public interface FeignClient {
+    @LoadBalanced
+    @GetMapping("api/inventory/exists")
     public List<InventoryResponse> isProductExist(@RequestParam("skuCodes") List<String> skuCodes);
 
 
--- Order-Service/src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
+++ Order-Service/src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
@@ -2,11 +2,13 @@ package com.ecommerce.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
