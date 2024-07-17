c92541422f5fbec7e986ba2d29e4d78fcd9d13bf
choudharysumit011 <csumit408@gmail.com>
2024-07-17 11:22:19 +0530

    test

--- auth-service/src/main/java/com/ecommerce/authservice/Controller/AuthController.java
+++ auth-service/src/main/java/com/ecommerce/authservice/Controller/AuthController.java
@@ -3,19 +3,23 @@ package com.ecommerce.authservice.Controller;
 import com.ecommerce.authservice.Model.UserCredentials;
 import com.ecommerce.authservice.Service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
+
+@Slf4j
 public class AuthController {
-    private final AuthService authService;
+    @Autowired
+     AuthService authService;
 
     @PostMapping("/register")
-    public String addNewUser(UserCredentials userCredentials){
+    public String addNewUser(@RequestBody UserCredentials userCredentials){
+        log.debug("User credentials: {}", userCredentials.getName());
+        log.debug("User password: {}", userCredentials.getPassword());
        return authService.userCreate(userCredentials);
 
 
@@ -24,6 +28,12 @@ public class AuthController {
     @GetMapping("/token")
     public String getToken(UserCredentials userCredentials){
         return authService.generateToken(userCredentials.getName());
+    }
+
+    @GetMapping("/validate")
+    public String validateToken(@RequestParam("token") String token){
+        authService.validateToken(token);
+        return "token valid";
     }
 
 
--- auth-service/src/main/java/com/ecommerce/authservice/Service/AuthService.java
+++ auth-service/src/main/java/com/ecommerce/authservice/Service/AuthService.java
@@ -4,17 +4,22 @@ import com.ecommerce.authservice.Config.AuthConfig;
 import com.ecommerce.authservice.Model.UserCredentials;
 import com.ecommerce.authservice.Repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+
 public class AuthService {
     private final AuthRepository authRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
 
     public String userCreate(UserCredentials userCredentials) {
+        if(userCredentials.getPassword() == null){
+            throw new IllegalArgumentException("Password cannot be null");
+        }
         userCredentials.setPassword(passwordEncoder.encode(userCredentials.getPassword()));
         authRepository.save(userCredentials);
         return "User created";
